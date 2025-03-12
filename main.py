b5ec6d17c9d679355149b38af67b8f52d02342a8
tinaba96 <tinaba96.ca@gmail.com>
2025-03-11 23:17:58 -0400

    update

--- main.py
+++ main.py
@@ -6,3 +6,16 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    return {"item_id": item_id}
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
