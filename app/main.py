294a975b6adac6bc2e0ad23a44d57862bcc5ca24
tinaba96 <tinaba96.ca@gmail.com>
2025-05-02 23:27:49 -0400

    update

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 # CORSミドルウェアの設定
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000"], # 一旦ローカルのみ対応
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
