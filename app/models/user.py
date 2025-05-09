6c12883d6b91bcb159542743ecc1c84d5ec8c8f8
tinaba96 <tinaba96.ca@gmail.com>
2025-05-08 23:48:01 -0400

    update

--- app/models/user.py
+++ app/models/user.py
@@ -1,24 +1,20 @@
-from pydantic import BaseModel
 from typing import Optional
-
+from pydantic import BaseModel
 
 class UserBase(BaseModel):
     name: str
     email: str
 
-
 class UserCreate(UserBase):
     password: str
-
+    id: Optional[str] = None  # `id`を任意のフィールドとして設定
 
 class User(UserBase):
-    id: int
+    id: str  # データベースから返却される際には必須
     created_at: str
 
     class Config:
         orm_mode = True
-
-
 class UserUpdate(BaseModel):
     name: Optional[str] = None
     email: Optional[str] = None
